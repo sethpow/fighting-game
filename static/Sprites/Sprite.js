48f4a9e1c2e66c6b609341419003f5b3dce032b0
sethpow <sethmadspowelson@gmail.com>
2022-12-19 23:48:04 -0700

    draw fn

--- static/Sprites/Sprite.js
+++ static/Sprites/Sprite.js
@@ -1,8 +1,17 @@
+// Imports
+import c from '../context.js'
+
 // Sprite
 class Sprite {
-    constructor(name, position) {
+    constructor(name, position, color) {
         this.name = name
         this.position = position
+        this.color = color
+    }
+
+    draw() {
+        c.fillStyle = this.color
+        c.fillRect(this.position.x, this.position.y, 50, 150)
     }
 }
 
