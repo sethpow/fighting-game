b29731e4986aff31450dd7348152d41513a848ca
sethpow <sethmadspowelson@gmail.com>
2022-12-20 00:36:58 -0700

    add velocity & gravity

--- static/Sprites/Sprite.js
+++ static/Sprites/Sprite.js
@@ -1,5 +1,9 @@
 // Imports
+import canvas from '../canvas.js'
 import c from '../context.js'
+
+// Global Variables
+const gravity = 0.2
 
 // Sprite
 class Sprite {
@@ -8,16 +12,23 @@ class Sprite {
         this.position = position
         this.velocity = velocity
         this.color = color
+        this.height = 150
     }
 
     draw() {
         c.fillStyle = this.color
-        c.fillRect(this.position.x, this.position.y, 50, 150)
+        c.fillRect(this.position.x, this.position.y, 50, this.height)
     }
 
     update() {
         this.draw()
-        this.position.y += 10
+        this.position.y += this.velocity.y
+
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y = 0
+        } else {
+            this.velocity.y += gravity
+        }
     }
 }
 
